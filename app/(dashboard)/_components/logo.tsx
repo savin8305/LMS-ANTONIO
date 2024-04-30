871e976b9542904ae7ed413520fbc8824cf316b5
savin8305 <kanhaji0402@gmail.com>
2024-04-30 13:07:19 +0530

    update

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Image
-      height={130}
-      width={130}
-      alt="logo"
-      src="/logo.svg"
-    />
-  )
-}
+    <div style={{ fontFamily: "Arial, sans-serif" }}>
+      <h1 style={{ fontWeight: "bold", fontSize: "24px", margin: "0" }}>LMS</h1>
+    </div>
+  );
+};
